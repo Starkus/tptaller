02ac9fb1380f377d98e0b703b4879ab46b777433
Starkus <emilianoalejosenega@gmail.com>
2018-10-31 09:49:01 -0300

    .hs original

---
+++ tp.hs
@@ -0,0 +1,45 @@
+type Conjunto a = [a]
+type Tablero = [[Integer]]
+type Posicion = (Integer,Integer)
+type Camino = [Posicion]
+
+sopa1 :: Tablero
+sopa1 = [[13, 12, 19, 6], [7, 13, 32, 6], [22, 20, 14, 7], [7, 33,53, 16], [27, 2, 8, 18]]
+
+sopa2 :: Tablero
+sopa2 = [[(-20), (-20), (-20)], [0, 10, 20], [(-10), (-10), 0], [10, 20,(-10)]]
+
+sopa3 :: Tablero
+sopa3 = [[10,5,15],[-1,7,2],[2,12,3]]
+
+camino1 :: Camino 
+camino1 = [(1,1),(1,2),(2,2),(2,3)]
+
+camino2 :: Camino 
+camino2 = [(2,1),(2,2),(2,3),(3,3),(4,3)]
+
+camino3 :: Camino 
+camino3 = [(1,2),(2,2),(3,2)]
+
+-- Dado la cantidad filas de un tablero.
+cantidadFilas :: Tablero -> Integer
+cantidadFilas t = fromIntegral (length t)
+
+-- Dado la cantidad columnas de un tablero.
+cantidadColumnas :: Tablero -> Integer
+cantidadColumnas (t:ts) = fromIntegral (length t)
+
+-- Devuelve el valor de una posicion de un tablero
+valor :: Tablero -> Posicion -> Integer
+valor (t:ts) (1,y) = valorY t y
+valor (t:ts) (x,y) = valor ts (x-1,y)
+
+valorY :: [Integer] -> Integer -> Integer
+valorY (c:cs) 1 = c
+valorY (c:cs) n = valorY cs (n-1)  
+
+-- Determina si una posicion esta dentro de los limites de un tablero
+posValida :: Tablero -> Posicion -> Bool
+posValida t (x,y) = x >= 1 && x <= (cantidadFilas t) && y >= 1 && y <= (cantidadColumnas t) 
+
+
